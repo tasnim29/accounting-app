871450ecc03b3fedff767ce52e1643330b78b998
tasnim29 <tasnimmahmudnjr@gmail.com>
2026-01-02 12:09:08 +0600

    db connected

---
+++ app/api/test-connection/route.js
@@ -0,0 +1,21 @@
+import pool from "@/lib/db";
+
+export async function GET(req) {
+  try {
+    const result = await pool.query("SELECT * FROM accounts");
+
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(
+      JSON.stringify({ error: "Database connection failed" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
